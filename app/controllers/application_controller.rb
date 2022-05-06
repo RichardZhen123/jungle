088fe31aff27b023b0900d8f2ff992c18a342da1
RichardZhen123 <richardzhen5555@gmail.com>
2022-05-06 21:44:29 +0000

    fixing http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   
-  http_basic_authenticate_with name: 'jungle', password: 'book'
-
   protect_from_forgery with: :exception
 
   private
@@ -32,3 +30,4 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 end
+
